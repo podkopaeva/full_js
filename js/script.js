8e5e09ecc0059d0b314630f796554e75ef246396
Iulia Kryshtal <podkopaeva_yuliya@ukr.net>
2018-04-26 23:49:53 +0300

    functions

---
+++ js/script.js
@@ -0,0 +1,26 @@
+// functions
+// var output = document.getElementById('output');
+
+
+// var hello = 'hello';
+
+// function helloW() {
+// 	var b = hello + 'World';
+// 	console.log(b);
+// }
+
+// helloW();
+
+// output.innerHTML = myFun1(4, 5, 6);
+// var response = "55"; // global variable
+// myFun1();
+
+// function myFun1(a, b, c) {
+// 	var response = a+ b + c; // global variable
+// 	return response;
+// }
+// function myFunc(){
+// 	output.innerHTML = 'hello </br>';
+// }
+
+// myFunc();
